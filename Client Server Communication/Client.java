b1928399846528b14581f552fbbd25632cb9e832
SUTHARSON1996 <1996sutharson@gmail.com>
2017-09-21 23:44:00 +0530

    added DNS Program

--- Client Server Communication/Client.java
+++ Client Server Communication/Client.java
@@ -5,6 +5,9 @@ import java.util.*;
 public class Client{
 
 	public static void main(String[] args)throws Throwable{
+		Socket soc = new Socket("localhost",3000);
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+		PrintStream ps = new PrintStream(soc.getOutputStream());
 		Scanner s = new Scanner(System.in);
 		String input="",output="";
 
@@ -13,18 +16,15 @@ public class Client{
 		while(!output.equals("bye")){
 			System.out.print("Client message = ");
 			output = s.nextLine();
-			Socket soc = new Socket("localhost",3000);
-			//BufferedReader buffer = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-			PrintStream ps = new PrintStream(soc.getOutputStream());
-
 			ps.println(output);
-			//input = buffer.readLine();
-			//System.out.println("Server message = "+input);
-			ps.close();
-			//buffer.close();
-			soc.close();
-
-		}
+			input = buffer.readLine();
+			System.out.println("Server message = "+input);
 		
+		}
+		ps.close();
+		buffer.close();
+		soc.close();
+
 	}
 }
+
